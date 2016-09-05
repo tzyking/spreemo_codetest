b4bd4a5e3624fbe0ca7b36cc38c3977772db0658
JeffreyTong <tongzheyi@gmail.com>
2016-09-05 00:35:11 -0400

    style fix

--- spec/mailers/appointment_mailer_spec.rb
+++ spec/mailers/appointment_mailer_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 describe AppointmentMailer do
   describe '#appointment_scheduled_notification' do
-    let(:doctor)            { create :doctor, specialty: 'Orthopedist' }
-    let(:patient)           { create :patient, ailment: 'broken bones' }
-    let(:appointment)       { create :appointment, doctor: doctor, patient: patient }
-    let(:mail)              { AppointmentMailer.appointment_scheduled_notification(appointment) }
-    let(:expected_mail_body) { "Doctor: #{doctor.name}. Patient: #{patient.name}. Time: #{appointment.formatted_appointment_date}. Address: #{doctor.address}." }
+    let(:doctor)              { create :doctor, specialty: 'Orthopedist' }
+    let(:patient)             { create :patient, ailment: 'broken bones' }
+    let(:appointment)         { create :appointment, doctor: doctor, patient: patient }
+    let(:mail)                { AppointmentMailer.appointment_scheduled_notification(appointment) }
+    let(:expected_mail_body)  { "Doctor: #{doctor.name}. Patient: #{patient.name}. Time: #{appointment.formatted_appointment_date}. Address: #{doctor.address}." }
 
     it 'sends mail to doctor and patient' do
       expect(mail.to).to eq([doctor.email, patient.email])
